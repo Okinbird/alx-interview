ccc65e75b00ad4777f38343b47af5dd8a368de6a
Okinbird <okinyinka@yahoo.co.uk>
2023-06-22 12:54:46 -0700

    making change correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
-"""
-Task: Change comes from within
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
-"""
+""" Making Change module """
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-    Return: fewest number of coins needed to meet total
-        - If total is 0 or less, return 0
-        - If total cannot be met by any number of coins you have, return -1
-        - Coins is a list of the values of the coins in your possession
-        - The value of a coin will always be an integer greater than 0
-        - You can assume you have an infinite number of each denomination of
-        coin in the list
-    """
+    """ makeChange: finds lowest number of coins for a given sum """
     if total <= 0:
         return 0
-
-    newVal = total + 1
-    store = {0: 0}
-
-    for i in range(1, total + 1):
-        store[i] = newVal
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
-        return -1
-
-    return store[total]
+    change = 0
+    num_coins = 0
+    index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    while(change < total and index < len(coins)):
+        if (change + sorted_coins[index] <= total):
+            change += sorted_coins[index]
+            num_coins += 1
+        else:
+            index += 1
+    if change == total:
+        return num_coins
+    return -1
